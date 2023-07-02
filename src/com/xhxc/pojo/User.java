07992f5a0407a63ecde457a821a35d9ce486f424
OVELYB <1113102202@qq.com>
2023-07-02 19:03:56 +0800

    修改User.java文件

--- src/com/xhxc/pojo/User.java
+++ src/com/xhxc/pojo/User.java
@@ -1,4 +1,104 @@
 package com.xhxc.pojo;
 
+import java.util.Date;
+
 public class User {
+    private  int id;
+    private String username;
+    private String password;
+    private String phone;
+    private String email;
+    private String address;
+    private int active;
+    private Date creaTime;
+    private int role;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getActive() {
+        return active;
+    }
+
+    public void setActive(int active) {
+        this.active = active;
+    }
+
+    public Date getCreaTime() {
+        return creaTime;
+    }
+
+    public void setCreaTime(Date creaTime) {
+        this.creaTime = creaTime;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                ", active=" + active +
+                ", creaTime=" + creaTime +
+                ", role=" + role +
+                '}';
+    }
 }
