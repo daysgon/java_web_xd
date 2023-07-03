315df3a5889bd1985e2dede5f70f888e44591f5f
OVELYB <1113102202@qq.com>
2023-07-03 10:57:49 +0800

    修改了pojo.User.java

--- src/com/xhxc/web/LoginWeb.java
+++ src/com/xhxc/web/LoginWeb.java
@@ -1,4 +1,20 @@
+//点击登陆之后跳转登陆界面
+
 package com.xhxc.web;
 
-public class LoginWeb {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginWeb extends HttpServlet {
+
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/login.jsp").forward(req,resp);
+    }
 }
