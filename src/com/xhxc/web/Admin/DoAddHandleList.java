65a9d284989b70ded554e93696b5942091446ccf
OVELYB <1113102202@qq.com>
2023-07-04 14:13:36 +0800

    修改了服务器不能启动的bug

--- src/com/xhxc/web/Admin/DoAddHandleList.java
+++ src/com/xhxc/web/Admin/DoAddHandleList.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("admin/doaddhandlelist")
+@WebServlet("/admin/doaddhandlelist")
 public class DoAddHandleList extends HttpServlet {
     private HandleDao dao=new HandleDao();
 
